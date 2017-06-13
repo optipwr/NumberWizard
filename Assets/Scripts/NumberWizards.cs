f3736ea4896bb1baa0627a82743136c9827a8a10
Michael Irby <michael.irbstoomuch@gmail.com>
2017-06-13 09:08:44 -0400

    init commit

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me!");
+		
+		int max = 1000;
+		int min = 1;
+		
+		print ("The highest number you can pick is " + max);
+		print ("The lowest number you can pick is " + min);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
